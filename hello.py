18314fb49e0615fc4763da8a3f7929b09547401d
ruslan-Aspire <ruslan.a.smart.80@gmail.com>
2019-11-06 15:15:52 +0200

    hello/0.4

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-from flask import Flask, Markup, abort, escape, flash, get_flashed_messages, jsonify, make_response, redirect, request, render_template, session, url_for
+#!venv/bin/python3
+
+from flask import Flask, Markup, abort, escape, flash, get_flashed_messages, jsonify, make_response, redirect, render_template, request, session, url_for
 from flask_bootstrap import Bootstrap
 
 import requests, os, re
@@ -28,6 +30,7 @@ def unauthorized(error):
 @app.errorhandler(404)
 def page_not_found(error):
 	return "This page does not exist".upper(), 404
+	# return {'error': 'Not found'}
 
 
 def valid_login(login, psw):
@@ -61,7 +64,6 @@ def index():
 
 	return render_template('index.html')
 
-
 @app.route('/login')
 @app.route('/login/<name>')
 def login(name=None):
@@ -84,22 +86,22 @@ def search():
 	query = request.args.get('query', '')
 
 	if query == "api":
-		return redirect(url_for('api'))
-
-	elif query == "cookies":
-		return redirect(url_for('cookies'))
+		return redirect(url_for('api_tasks'))
 
 	elif query == "post":
 		return redirect(url_for('post'))
-
-	elif query == "session":
-		return redirect(url_for('view_session'))
 
 	elif query == "teapot":
 		return redirect(url_for('teapot'))
 
 	elif query == "upload":
 		return redirect(url_for('upload_file'))
+
+	elif query == "cookies":
+		return redirect(url_for('cookies'))
+
+	elif query == "session":
+		return redirect(url_for('view_session'))
 
 	elif query == "clear cookies":
 		resp = make_response("Cookies have been cleared")
@@ -122,24 +124,9 @@ def process_data(file):
 	table.columns = ['girls', 'boys']
 	table.index.name = None
 
-	table.plot(
-		figsize=(12, 5.5), 
-		fontsize=14, 
-		color=['tab:red', 'tab:blue'], 
-		style=['-s', '-s'], 
-		rot=None, 
-
-		linewidth=2, 
-		markersize=6, 
-		markerfacecolor='w', 
-		markeredgecolor=None, 
-		markeredgewidth=1, 
-
-		layout=(1, 1), 
-		subplots=False, 
-		grid=True, 
-		legend=True, 
-		)
+	table.plot(figsize=(12, 5.5), fontsize=14, color=['tab:red', 'tab:blue'], style=['-s', '-s'], rot=None, 
+		linewidth=2, markersize=6, markerfacecolor='w', markeredgecolor=None, markeredgewidth=1, 
+		layout=(1, 1), subplots=False, grid=True, legend=True)
 
 	plt.legend(loc='upper left', fontsize=14, borderpad=1, labelspacing=1, ncol=1, edgecolor='darkblue')
 
@@ -223,16 +210,11 @@ def env():
 	return "Hello! " * times
 
 
-@app.route('/api')
-def api():
-	return jsonify({"username": "ruslan", "password": 123456})
-
-
 @app.route('/post', methods=['GET', 'POST'])
 def post():
 	if request.method == 'POST':
 		return request.form
-	return render_template('api.html')
+	return render_template('post.html')
 
 
 @app.route('/cookies')
@@ -245,8 +227,203 @@ def view_session():
 	return escape(session)
 
 
+#------------------------------HTTPBasicAuth------------------------------
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import hashlib
+auth = HTTPBasicAuth()
+
+
+# users = {
+# 	"john": generate_password_hash("hello"),
+# 	"susan": generate_password_hash("bye")
+# }
+
+
+# @auth.verify_password
+# def verify_password(username, password):
+# 	if username in users:
+# 		return check_password_hash(users.get(username), password)
+# 	return False
+
+
+def hash_password(password):
+	salt = os.urandom(32)
+	key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+	return salt + key
+
+
+users = {
+	"john": hash_password("hello"),
+	"susan": hash_password("bye")
+}
+
+
+# @auth.verify_password
+# def verify_password(username, password):
+# 	if username in users:
+# 		salt = users[username][:32]
+# 		key = users[username][32:]
+# 		new_key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+# 		if new_key == key:
+# 			return True
+# 	return False
+
+
+@auth.get_password
+def get_password(username):
+	return users.get(username)
+
+
+@auth.hash_password
+def hash_pw(username, password):
+	if username in users:
+		salt = users[username][:32]
+		key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+		return salt + key
+
+
+@auth.error_handler
+def unauthorized():
+	return {'error': 'Unauthorized access'}, 401
+#------------------------------HTTPBasicAuth------------------------------
+
+
+#------------------------------RESTful API------------------------------
+tasks = [
+	{
+		'id': 1,
+		'title': 'Buy groceries',
+		'description': 'Milk, Cheese, Pizza, Fruit, Tylenol', 
+		'done': False
+	},
+	{
+		'id': 2,
+		'title': 'Learn Python',
+		'description': 'Need to find a good Python tutorial on the web', 
+		'done': False
+	}
+]
+
+
+def make_public_task(task):
+	new_task = {}
+	for field in task:
+		if field == 'id':
+			new_task['uri'] = url_for('api_tasks', task_id=task['id'], _external=True)
+		else:
+			new_task[field] = task[field]
+	return new_task
+
+
+@app.route('/api/tasks', methods=['GET', 'POST'])
+@app.route('/api/tasks/<int:task_id>', methods=['GET', 'PUT', 'DELETE'])
+@auth.login_required
+def api_tasks(task_id=None):
+
+#------------------------------POST------------------------------
+	if request.method == 'POST':
+		if not request.json or 'title' not in request.json:
+			abort(400)
+
+		task = {
+			'id': tasks[-1]['id'] + 1,
+			'title': request.json['title'],
+			'description': request.json.get('description', ""),
+			'done': False
+		}
+
+		tasks.append(task)
+		return {'task': task}, 201
+	
+	task = list(filter(lambda t: t['id'] == task_id, tasks))
+
+#------------------------------PUT------------------------------
+	if request.method == 'PUT':
+		if len(task) == 0:
+			abort(404)
+
+		if not request.json:
+			abort(400)
+
+		if 'title' in request.json and type(request.json['title']) is not str:
+			abort(400)
+
+		if 'description' in request.json and type(request.json['description']) is not str:
+			abort(400)
+
+		if 'done' in request.json and type(request.json['done']) is not bool:
+			abort(400)
+
+		task[0]['title'] = request.json.get('title', task[0]['title'])
+		task[0]['description'] = request.json.get('description', task[0]['description'])
+		task[0]['done'] = request.json.get('done', task[0]['done'])
+		return {'task': task[0]}
+
+#------------------------------DELETE------------------------------
+	if request.method == 'DELETE':
+		if len(task) == 0:
+			abort(404)
+
+		tasks.remove(task[0])
+		return {'result': True}
+
+#------------------------------GET------------------------------
+	if not task_id:
+		return {'tasks': list(map(make_public_task, tasks))}
+
+	if len(task) == 0:
+		abort(404)
+
+	return jsonify({'task': make_public_task(task[0])})
+
+
+'''
+RESTful API:
+curl -I -u john:hello http://localhost:5000/api/tasks
+curl -i -u john:hello http://localhost:5000/api/tasks
+curl -i -u john:hello http://localhost:5000/api/tasks/1
+curl -i -u john:hello -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/api/tasks
+curl -i -u john:hello -H "Content-Type: application/json" -X PUT -d '{"done":true}' http://localhost:5000/api/tasks/3
+curl -i -u john:hello -X DELETE http://localhost:5000/api/tasks/3
+'''
+
+
+'''
+import requests
+
+MAKE A REQUEST
+auth = ("john", "hello")
+headers = {"Content-Type": "application/json"}
+
+r = requests.get("http://localhost:5000/api/tasks", auth=auth)
+r = requests.post("http://localhost:5000/api/tasks", auth=auth, headers=headers, data='{"title": "Read a book"}')
+r = requests.put("http://localhost:5000/api/tasks/3", auth=auth, headers=headers, data='{"done": true}')
+r = requests.delete("http://localhost:5000/api/tasks/3", auth=auth)
+
+POST MULTIPART-ENCODED FILES
+files = {
+    "file1": open("data/file1.jpg", "rb"), 
+    "file2": open("data/file2.jpg", "rb")
+}
+
+r = requests.post(url, auth=auth, files=files)
+# Flask: request.files["file1"]
+
+print("Status code:", r.status_code, end="\n\n")
+print("Response Headers:", r.headers, sep="\n", end="\n\n")
+print("JSON Response Content:", r.json(), sep="\n")
+print("JSON Response Content:", r.text, sep="\n")
+'''
+
+
 # with app.test_request_context():
 # 	print(url_for('index'))
 # 	print(url_for('login'))
 # 	print(url_for('login', name='John Doe'))
 # 	print(url_for('static', filename='style.css'))
+# 	print(url_for('api_tasks', task_id=1, _external=True))
+
+
+# if __name__ == '__main__':
+#   app.run(debug=True)
